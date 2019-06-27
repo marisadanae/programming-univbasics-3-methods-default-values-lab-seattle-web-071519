104879e16a566df8c981a27ee9e418c5418c34e3
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-06-27 04:52:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,11 +1,12 @@
 def meal_choice(veg1, veg2, protein = "meat")
   if meal_choice == protein
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+elsif 
   meal_choice = veg
   puts "A plate of #{veg1} and #{veg2}."
 end
   puts "What a nutritious meal!"
-end
+  
 meal_choice("broccoli", "macaroni", "tofu")
 meal_choice("broccoli", "macaroni", "meat")
 meal_choice("broccoli", "macaroni")
