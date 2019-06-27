4da7cd0dd2fa89abb1a42bae0a8327a4b2724f4d
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-06-27 03:08:48 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
 def meal_choice(veg1, veg2, protein = "meat")
   meal_choice = protein
   puts "A plate of {veg1} and {veg2}."
-if 
+if
   puts "A plate of {protein} with {veg1} and {veg2}."
 else
   puts("What a nutritious meal!")
