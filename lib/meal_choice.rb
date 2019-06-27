ff209a1568c15d1997424a6fe126708d15006197
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-06-27 04:36:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,11 +1,9 @@
 def meal_choice(veg1, veg2, protein = "meat")
-  meal_choice = protein
-  puts "A plate of {veg1} and {veg2}."
-if
-  puts "A plate of {protein} with {veg1} and {veg2}."
-else
-  puts("What a nutritious meal!")
-
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  meal_choice = vegetables
+  puts "A plate of #{veg1} and #{veg2}."  
+end
+  puts "What a nutritious meal!"
 end
 
 meal_choice("broccoli", "macaroni")
