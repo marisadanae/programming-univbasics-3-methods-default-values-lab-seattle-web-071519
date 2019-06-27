1d124a7a13c664daecba9709926e79c647981810
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-06-27 04:52:39 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,12 @@
-
-def meal_choice(veg1, veg2, meat = "protein")
-  puts "What a nutritious meal!"
-  if meal_choice == "meat"
+def meal_choice(veg1, veg2, protein = "meat")
+  if meal_choice == protein
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
+elsif
+  meal_choice = veg
+  puts "A plate of #{veg1} and #{veg2}."
 end
-meal_choice("broccoli", "macaroni")
+  puts "What a nutritious meal!"
+  
 meal_choice("broccoli", "macaroni", "tofu")
+meal_choice("broccoli", "macaroni", "meat")
+meal_choice("broccoli", "macaroni")
